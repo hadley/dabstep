90c43ddb038370467c13158614f00850c9f0f8dd
Hadley Wickham <h.wickham@gmail.com>
2026-03-10 08:42:35 -0500

    Add in merchant data

--- 02-convert-data.R
+++ 02-convert-data.R
@@ -14,6 +14,18 @@ merchant_category_codes <- merchant_category_codes[
 ]
 write_parquet(merchant_category_codes, "data/merchant_category_codes.parquet")
 
+merchant_data <- jsonlite::fromJSON("raw-data/merchant_data.json")
+merchants <- merchant_data[, c("merchant", "capture_delay", "merchant_category_code", "account_type")]
+merchants <- merchants[order(merchants$merchant), ]
+write_parquet(merchants, "data/merchants.parquet")
+
+merchant_acquirers <- data.frame(
+  merchant = rep(merchant_data$merchant, lengths(merchant_data$acquirer)),
+  acquirer = unlist(merchant_data$acquirer)
+)
+merchant_acquirers <- merchant_acquirers[order(merchant_acquirers$merchant, merchant_acquirers$acquirer), ]
+write_parquet(merchant_acquirers, "data/merchant_acquirers.parquet")
+
 payments <- read.csv("raw-data/payments.csv")
 bool_cols <- grep("^(is_|has_)", names(payments), value = TRUE)
 payments[bool_cols] <- lapply(payments[bool_cols], \(x) x == "True")
